06c732f4aeb4e1efb5f2b9ff51dcd5eaf5b99ce6
gwatch <gwatch@example.com>
2026-04-17 16:50:29 -0300

    gwatch auto-commit

--- Enums/MidiaTipo.cs
+++ Enums/MidiaTipo.cs
@@ -4,9 +4,13 @@ namespace Telinha.Enums
 {
     public enum MidiaTipo
     {
+        [Description("Filme")]
         Filme,
+
         [Description("Série")]
         Serie,
+
+        [Description("Anime")]
         Anime
     }
 }
