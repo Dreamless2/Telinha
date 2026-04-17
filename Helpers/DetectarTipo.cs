8ffe15c5d689e4b76640639fd5cc438a99898021
gwatch <gwatch@example.com>
2026-04-17 16:44:41 -0300

    gwatch auto-commit

--- Helpers/DetectarTipo.cs
+++ Helpers/DetectarTipo.cs
@@ -1,11 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
-using Telinha.Enums;
+﻿using System.ComponentModel;
 
-namespace Telinha.Entity
+namespace Telinha.Helpers
 {
     public class GenericHelpers
     {
