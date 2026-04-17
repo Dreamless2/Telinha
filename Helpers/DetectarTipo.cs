ce0a174471b06fd4f58b94e3203f629962925a97
gwatch <gwatch@example.com>
2026-04-17 16:44:29 -0300

    gwatch auto-commit

--- Helpers/DetectarTipo.cs
+++ Helpers/DetectarTipo.cs
@@ -9,13 +9,13 @@ namespace Telinha.Entity
 {
     public class GenericHelpers
 
-
+    {
 
         public static string GetDescription(Enum value)
-    {
-        var field = value.GetType().GetField(value.ToString());
-        var attr = (DescriptionAttribute?)Attribute.GetCustomAttribute(field!, typeof(DescriptionAttribute));
-        return attr?.Description ?? value.ToString();
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attr = (DescriptionAttribute?)Attribute.GetCustomAttribute(field!, typeof(DescriptionAttribute));
+            return attr?.Description ?? value.ToString();
+        }
     }
-}
 }
