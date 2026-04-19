a554d5a67732a439008a08ba474a0c201e60d272
Dreamless2 <tiagonet50@gmail.com>
2026-04-19 18:52:38 -0300

    TokenFactory.cs

--- Factory/TokenFactory.cs
+++ Factory/TokenFactory.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Telinha.Data;
+﻿using Telinha.Data;
 using Telinha.Helpers;
 using Telinha.Models;
 using Telinha.Services;
