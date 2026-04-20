b8c742dd719074ece1c95e518c212575d83677b9
Dreamless2 <tiagonet50@gmail.com>
2026-04-19 22:13:25 -0300

    DEEPLContracts.cs

--- Services/TMDBServices.cs
+++ Services/TMDBServices.cs
@@ -3,32 +3,31 @@ using RestSharp;
 
 namespace Telinha.Services
 {
-    public class TMDBServices(string token)
+    public class TMDBServices
     {
-        private readonly RestClient _client = new("https://api.themoviedb.org/3/");
-        private readonly string _token = token;
+        private readonly RestClient _client;
+
+        public TMDBServices(RestClient client)
+        {
+            _client = client;
+        }
 
         public async Task<JObject> GetAsync(string endpoint, Dictionary<string, string>? query = null)
         {
             var req = new RestRequest(endpoint);
 
             req.AddHeader("accept", "application/json");
-            req.AddHeader("Authorization", $"Bearer {_token}");
 
             if (query != null)
             {
                 foreach (var p in query)
-                {
                     req.AddQueryParameter(p.Key, p.Value);
-                }
             }
 
             var resp = await _client.ExecuteAsync(req);
 
             if (!resp.IsSuccessful || string.IsNullOrWhiteSpace(resp.Content))
-            {
                 throw new Exception($"TMDB ERROR: {endpoint} {resp.StatusCode}");
-            }
 
             return JObject.Parse(resp.Content!);
         }
