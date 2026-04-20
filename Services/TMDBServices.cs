a3b67faa62c1a703623c51b29748be3bcdee816d
Dreamless2 <tiagonet50@gmail.com>
2026-04-19 22:41:44 -0300

    TMDBServices.cs

--- Services/TMDBServices.cs
+++ Services/TMDBServices.cs
@@ -6,13 +6,14 @@ namespace Telinha.Services
     public class TMDBServices()
     {
         private readonly string _token;
+        private readonly RestClient _client = client;
+
 
         public TMDBServices(RestClient client, string token)
         {
             _client = client;
             _token = token;
         }
-        private readonly RestClient _client = client;
 
         public async Task<JObject> GetAsync(string endpoint, Dictionary<string, string>? query = null)
         {
