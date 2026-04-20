1fd5e47ea756ef65b00eafd60b5c4ad6ad9a0f6d
Dreamless2 <tiagonet50@gmail.com>
2026-04-19 22:10:32 -0300

    APIClientFactory.cs

--- Contracts/DEEPLContracts.cs
+++ Contracts/DEEPLContracts.cs
@@ -7,16 +7,9 @@ namespace Telinha.Contracts
     {
         private readonly DeepLClient _client;
 
-        public DEEPLContracts()
+        public DEEPLContracts(DeepLClient client)
         {
-            var deeplKey = "7feb3eb8-de95-4312-843c-1064aecdab8b:fx";
-
-            if (string.IsNullOrWhiteSpace(deeplKey))
-            {
-                throw new InvalidOperationException("Chave API do DEEPL não configurado.");
-            }
-
-            _client = new DeepLClient(deeplKey);
+            _client = client;
         }
 
         public async Task<TextResult> Translate(string text) =>
