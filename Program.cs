68fe99b5a3224ece9262d67e60852ea946be043e
Dreamless2 <tiagonet50@gmail.com>
2026-04-19 21:54:38 -0300

    Token.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Telinha
+﻿using Telinha.Services;
+
+namespace Telinha
 {
     internal static class Program
     {
@@ -6,6 +8,20 @@
         static void Main()
         {
             ApplicationConfiguration.Initialize();
+
+            var tokenService = new TokenServices();
+            var hasToken = Task.Run(async () =>
+            {
+                var tmdb = await tokenService.ObterTokenAsync("TMDB");
+                var deepl = await tokenService.ObterTokenAsync("DEEPL");
+
+                return !string.IsNullOrEmpty(tmdb) || !string.IsNullOrEmpty(deepl);
+            }).GetAwaiter().GetResult();
+
+
+
+
+
             Application.Run(new Token());
         }
     }
