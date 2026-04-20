212b4570ff5fbe74bbafc1bbece43527b869267a
Dreamless2 <tiagonet50@gmail.com>
2026-04-19 21:31:38 -0300

    TokenServices.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
-﻿namespace Telinha
+﻿using Telinha.Services;
+
+namespace Telinha
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.         
             ApplicationConfiguration.Initialize();
             Application.Run(new Home());
         }
