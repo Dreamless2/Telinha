a343b9667fcc9648f83faa0313ffa55689f2fc80
Dreamless2 <tiagonet50@gmail.com>
2026-04-19 22:42:14 -0300

    TMDBServices.cs

--- Services/TMDBServices.cs
+++ Services/TMDBServices.cs
@@ -5,8 +5,8 @@ namespace Telinha.Services
 {
     public class TMDBServices()
     {
-        private readonly string _token;
-        private readonly RestClient _client = client;
+        private readonly string? _token;
+        private readonly RestClient? _client;
 
 
         public TMDBServices(RestClient client, string token)
@@ -14,35 +14,6 @@ namespace Telinha.Services
             _client = client;
             _token = token;
         }
-
-        public async Task<JObject> GetAsync(string endpoint, Dictionary<string, string>? query = null)
-        {
-            var req = new RestRequest(endpoint);
-
-            req.AddHeader("accept", "application/json");
-
-            // 🔥 GARANTE o token aqui
-            req.AddHeader("Authorization", $"Bearer {_token}");
-
-            if (query != null)
-            {
-                foreach (var p in query)
-                    req.AddQueryParameter(p.Key, p.Value);
-            }
-
-            var resp = await _client.ExecuteAsync(req);
-
-            if (!resp.IsSuccessful || string.IsNullOrWhiteSpace(resp.Content))
-                throw new Exception($"TMDB ERROR: {endpoint} {resp.StatusCode}\n{resp.Content}");
-
-            return JObject.Parse(resp.Content!);
-        }
-
-        public async Task<JObject[]> Many(params (string url, Dictionary<string, string>? q)[] calls)
-        {
-            return await Task.WhenAll(
-                calls.Select(c => GetAsync(c.url, c.q))
-            );
-        }
     }
-}
+
+        
