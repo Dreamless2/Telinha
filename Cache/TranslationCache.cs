c441ec9126bc25621bc572548e82ee0f6415a0a6
Dreamless2 <tiagonet50@gmail.com>
2026-04-22 20:53:55 -0300

    DEEPLContracts.cs APIClientFactory.cs MidiaFactory.cs MidiaServices.cs

--- Cache/TranslationCache.cs
+++ Cache/TranslationCache.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Telinha.Cache
+﻿namespace Telinha.Cache
 {
     public static class TranslationCache
     {
--- Mapper/LanguageMapper.cs
+++ Mapper/LanguageMapper.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Telinha.Mapper
+﻿namespace Telinha.Mapper
 {
     public static class LanguageMapper
     {
