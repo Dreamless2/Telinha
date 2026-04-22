388ab76b4db41f99ccc0e339c2ee50fffcb47965
Dreamless2 <tiagonet50@gmail.com>
2026-04-22 19:33:25 -0300

    LanguageMapper.cs

--- Mapper/LanguageMapper.cs
+++ Mapper/LanguageMapper.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Telinha.Mapper
 {
-    internal class LanguageMapper
+    public static class LanguageMapper
     {
+        private static readonly Dictionary<string, string> Map = new(StringComparer.OrdinalIgnoreCase)
+        {
+            ["English"] = "Inglês",
+            ["Portuguese"] = "Português",
+            ["Spanish"] = "Espanhol",
+            ["French"] = "Francês",
+            ["German"] = "Alemão",
+            ["Italian"] = "Italiano",
+            ["Japanese"] = "Japonês",
+            ["Korean"] = "Coreano",
+            ["Chinese"] = "Chinês",
+            ["Russian"] = "Russo"
+        };
+
+        public static string ToPtBr(string englishName)
+        {
+            return Map.TryGetValue(englishName, out var pt) ? pt : englishName;
+        }
     }
 }
