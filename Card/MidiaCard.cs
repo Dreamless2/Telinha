9f88f2efeb7f6b1531d0c60db8da5e30d83529f0
Dreamless2 <tiagonet50@gmail.com>
2026-04-22 14:08:54 -0300

    TagEngine.cs

--- Card/MidiaCard.cs
+++ Card/MidiaCard.cs
@@ -3,69 +3,46 @@ using Telinha.Enums;
 
 namespace Telinha.Card
 {
-    public class MidiaCard
+    public class MidiaCard(
+        MidiaTipo tipo,
+        string titulo,
+        string audio = "",
+        string sinopse = "",
+        string original = "",
+        string lancamento = "",
+        string alternativo = "",
+        string pais = "",
+        string idioma = "",
+        string franquia = "",
+        string genero = "",
+        string tags = "",
+        string diretor = "",
+        string artistas = "",
+        string produtora = "",
+        string mcu = "",
+        string autores = "",
+        string criadores = "",
+        string obra = "")
     {
-        private readonly MidiaTipo _tipo;
-        private readonly string _titulo;
-        private readonly string _audio;
-        private readonly string _sinopse;
-        private readonly string _original;
-        private readonly string _lancamento;
-        private readonly string _alternativo;
-        private readonly string _pais;
-        private readonly string _idioma;
-        private readonly string _franquia;
-        private readonly string _genero;
-        private readonly string _tags;
-        private readonly string _diretor;
-        private readonly string _artistas;
-        private readonly string _produtora;
-        private readonly string _mcu;
-        private readonly string _autores;
-        private readonly string _criadores;
-        private readonly string _obra;
-
-        public MidiaCard(
-            MidiaTipo tipo,
-            string titulo,
-            string audio = "",
-            string sinopse = "",
-            string original = "",
-            string lancamento = "",
-            string alternativo = "",
-            string pais = "",
-            string idioma = "",
-            string franquia = "",
-            string genero = "",
-            string tags = "",
-            string diretor = "",
-            string artistas = "",
-            string produtora = "",
-            string mcu = "",
-            string autores = "",
-            string criadores = "",
-            string obra = "")
-        {
-            _tipo = tipo;
-            _titulo = titulo ?? "";
-            _audio = audio ?? "";
-            _sinopse = sinopse ?? "";
-            _original = original ?? "";
-            _lancamento = lancamento ?? "";
-            _alternativo = alternativo ?? "";
-            _pais = pais ?? "";
-            _idioma = idioma ?? "";
-            _franquia = franquia ?? "";
-            _genero = genero ?? "";
-            _tags = tags ?? "";
-            _diretor = diretor ?? "";
-            _artistas = artistas ?? "";
-            _produtora = produtora ?? "";
-            _mcu = mcu ?? "";
-            _autores = autores ?? "";
-            _criadores = criadores ?? "";
-            _obra = obra ?? "";
-        }
+        private readonly MidiaTipo _tipo = tipo;
+        private readonly string _titulo = titulo ?? "";
+        private readonly string _audio = audio ?? "";
+        private readonly string _sinopse = sinopse ?? "";
+        private readonly string _original = original ?? "";
+        private readonly string _lancamento = lancamento ?? "";
+        private readonly string _alternativo = alternativo ?? "";
+        private readonly string _pais = pais ?? "";
+        private readonly string _idioma = idioma ?? "";
+        private readonly string _franquia = franquia ?? "";
+        private readonly string _genero = genero ?? "";
+        private readonly string _tags = tags ?? "";
+        private readonly string _diretor = diretor ?? "";
+        private readonly string _artistas = artistas ?? "";
+        private readonly string _produtora = produtora ?? "";
+        private readonly string _mcu = mcu ?? "";
+        private readonly string _autores = autores ?? "";
+        private readonly string _criadores = criadores ?? "";
+        private readonly string _obra = obra ?? "";
 
         public string GetFormattedText()
         {
