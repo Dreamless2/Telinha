8a1313cd59e1af4ff3d41db8132d89c1e4f23dd4
Dreamless2 <tiagonet50@gmail.com>
2026-04-22 12:30:59 -0300

    MidiaServices.cs

--- Services/MidiaServices.cs
+++ Services/MidiaServices.cs
@@ -130,6 +130,8 @@ namespace Telinha.Services
 
             bool invalido = results[0]?["id"] == null || (tipo == MidiaTipo.Filme && string.IsNullOrWhiteSpace(results[0]?["title"]?.ToString())) || (tipo != MidiaTipo.Filme && string.IsNullOrWhiteSpace(results[0]?["name"]?.ToString()));
 
+            LogServices.Info("Busca de midia concluida: {Id} - {Tipo} - {Invalido}", id, tipo, invalido);
+
             if (invalido)
                 return null;
 
