d850588d6226b2deccb15293ddfa922f6c8db57d
Dreamless2 <tiagonet50@gmail.com>
2026-04-22 12:02:15 -0300

    LogServices.cs

--- Infrastructure/Logging/LogServices.cs
+++ Infrastructure/Logging/LogServices.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Telinha.Infrastructure.Logging
 {
-    internal class LogServices
+    public class LogServices
     {
+        public static void Configure()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log-.txt",
+                    rollingInterval: RollingInterval.Day,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            Log.Information("Log configurado com sucesso.");
+        }
+
+        // Método genérico para facilitar logs de erro com contexto
+        public static void Error(Exception ex, string message, params object[] args)
+        {
+            Log.Error(ex, message, args);
+        }
+
+        public static void Info(string message, params object[] args)
+        {
+            Log.Information(message, args);
+        }
+
+        public static void Close()
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
