9f9e4d1166c09e211cc917128d68df996088ef7f
Dreamless2 <tiagonet50@gmail.com>
2026-04-22 20:24:32 -0300

    TranslationCache.cs

--- Cache/TranslationCache.cs
+++ Cache/TranslationCache.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Telinha.Cache
 {
-    internal class TranslationCache
+    public static class TranslationCache
     {
+        private static readonly Dictionary<string, string> Cache = new(StringComparer.OrdinalIgnoreCase);
+
+        public static bool TryGet(string key, out string value)
+            => Cache.TryGetValue(key, out value!);
+
+        public static void Set(string key, string value)
+            => Cache[key] = value;
     }
 }
