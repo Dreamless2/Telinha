bbf746e7119060a7f1d0cb2f8c47c96d01fb7d40
Dreamless2 <tiagonet50@gmail.com>
2026-04-22 11:24:10 -0300

    MidiaServices.cs

--- Services/MidiaServices.cs
+++ Services/MidiaServices.cs
@@ -114,9 +114,9 @@ namespace Telinha.Services
 
             var apiFactory = new ApiClientFactory(_tokenService!);
 
-            var deepl = await apiFactory.GetDeepLAsync();
+            //var deepl = await apiFactory.GetDeepLAsync();
 
-            var model = await MidiaFactory.ConstruirMidia(results[0], results[1], results.Length > 2 ? results[2] : null, tipo, deepl);
+            var model = await MidiaFactory.ConstruirMidia(results[0], results[1], results.Length > 2 ? results[2] : null, tipo);
 
             if (model != null)
             {
