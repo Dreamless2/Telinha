b58c04a88059789bf163fa8d96eba4eafc1a9b4c
Dreamless2 <tiagonet50@gmail.com>
2026-04-22 12:45:58 -0300

    Functions.cs

--- Utils/Functions.cs
+++ Utils/Functions.cs
@@ -2,22 +2,6 @@
 {
     public class Functions
     {
-        public static void SetCamposEnabled(Control parent, bool enabled, params Type[] tipos)
-        {
-            foreach (Control ctrl in parent.Controls)
-            {
-                if (tipos.Length == 0 || tipos.Any(t => t.IsAssignableFrom(ctrl.GetType())))
-                {
-                    ctrl.Enabled = enabled;
-                }
-
-                if (ctrl.HasChildren)
-                {
-                    SetCamposEnabled(ctrl, enabled, tipos);
-                }
-            }
-        }
-
         public static void OnlyNumbers(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
