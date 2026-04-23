3464a52a94fff171c75fa5bedc08216791a702bc
Dreamless2 <tiagonet50@gmail.com>
2026-04-23 11:58:16 -0300

    Home.cs

--- Views/Home.cs
+++ Views/Home.cs
@@ -315,9 +315,22 @@ namespace Telinha
             }
         }
 
-        private void AnteriorButton_Click(object sender, EventArgs e)
+        private async void AnteriorButton_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var item = await MidiaController.GetPrevious<MidiaModel>(currentId);
 
+                if (item == null)
+                    throw new Exception("Não há mais registros.");
+
+                currentId = item.Id;
+                _bs.DataSource = item; // ou PreencherCampos(item)
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
 
