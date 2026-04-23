7b1e853929a6a336b8e1f9af3af148d7e75525e4
Dreamless2 <tiagonet50@gmail.com>
2026-04-23 12:03:29 -0300

    Home.cs

--- Views/Home.cs
+++ Views/Home.cs
@@ -27,6 +27,7 @@ namespace Telinha
             CopiarButton.Click += CopiarButton_Click!;
             CodigoBox.KeyPress += (s, e) => Functions.OnlyNumbers(s!, e);
             CodigoBox.KeyDown += BuscarMidia!;
+            CodigoBox.TextChanged += BuscarMidia!;
             SalvarButton.Click += SalvarButton_ClickAsync;
             AnteriorButton.Click += AnteriorButton_Click!;
             ProximoButton.Click += ProximoButton_Click!;
