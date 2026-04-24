45d9aad769774efb646e2b4fadae49cc652da73b
Dreamless2 <tiagonet50@gmail.com>
2026-04-24 17:15:33 -0300

    Home.cs

--- Views/Home.cs
+++ Views/Home.cs
@@ -15,7 +15,7 @@ namespace Telinha
         private TMDBServices? _tmdb;
         private MidiaServices? _midiaService;
         private readonly ApiClientFactory _apiFactory;
-        private readonly FileCacheServices _cacheServices;
+        private readonly FileCacheServices _cacheService;
 
         private long currentId = 0;
         private readonly BindingSource _bs = [];
