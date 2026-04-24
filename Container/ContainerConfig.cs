99593cbe151f78fdbeb8ef39653f49c0a5a34a2c
Dreamless2 <tiagonet50@gmail.com>
2026-04-24 17:49:22 -0300

    TMDBServices.cs

--- Container/ContainerConfig.cs
+++ Container/ContainerConfig.cs
@@ -24,7 +24,20 @@ namespace Telinha.Container
             // APIs
             builder.RegisterType<ApiClientFactory>().AsSelf().SingleInstance();
             builder.RegisterType<AppConfigServices>().AsSelf().SingleInstance();
-            builder.RegisterType<TMDBServices>().AsSelf();
+            // 🔥 TMDBServices com factory
+            builder.Register(c =>
+            {
+                var factory = c.Resolve<ApiClientFactory>();
+                var config = c.Resolve<AppConfigServices>().Load();
+
+                if (string.IsNullOrWhiteSpace(config.TMDB))
+                    throw new InvalidOperationException("Token TMDB não encontrado. Configure na tela de Token.");
+
+                var client = factory.GetTMDB(); // RestClient base
+                return new TMDBServices(client, config.TMDB); // passa os 2 params
+            })
+           .AsSelf()
+           .SingleInstance(); // TMDBServices pode ser singleton
             builder.RegisterType<MidiaServices>().AsSelf();
 
             // Forms
