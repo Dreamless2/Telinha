7a7f25709a7827b9f72fe4fef87ceb4374a3efb7
Dreamless2 <tiagonet50@gmail.com>
2026-04-23 22:56:41 -0300

    MidiaCache.cs

--- Services/MidiaServices.cs
+++ Services/MidiaServices.cs
@@ -116,11 +116,11 @@ namespace Telinha.Services
 
             var model = await MidiaFactory.ConstruirMidia(results[0], results[1], results.Length > 2 ? results[2] : null, tipo, deepl);
 
-            /*if (model != null)
-               /*if (Enum.TryParse(model.Tipo, out MidiaTipo tipoReal))
+            if (model != null)
+                if (Enum.TryParse(model.Tipo, out MidiaTipo tipoReal))
                     MidiaCache.Save(tipoReal, id, JsonConvert.SerializeObject(model));
                 else
-                    MidiaCache.Save(tipo, id, JsonConvert.SerializeObject(model));*/
+                    MidiaCache.Save(tipo, id, JsonConvert.SerializeObject(model));
 
             return model;
         }
