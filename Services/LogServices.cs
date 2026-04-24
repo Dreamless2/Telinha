b77939ca4f9b798c657e69ea4b439e569ba8c24c
Dreamless2 <tiagonet50@gmail.com>
2026-04-24 11:28:30 -0300

    MidiaServices.cs

--- Services/LogServices.cs
+++ Services/LogServices.cs
@@ -7,8 +7,12 @@ namespace Telinha.Services
 {
     public static class LogServices
     {
+        private static bool _configurado = false;
+
         public static void ConfigurarLog()
         {
+            if (_configurado) return;
+
             string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "log-.txt");
 
             Log.Logger = new LoggerConfiguration()
@@ -16,43 +20,45 @@ namespace Telinha.Services
                 .WriteTo.File(logPath,
                     rollingInterval: RollingInterval.Day,
                     retainedFileCountLimit: 7,
+                    fileSizeLimitBytes: 10_000_000,
                     outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
                 .CreateLogger();
 
+            _configurado = true;
             Log.Information("Sistema de logs inicializado com sucesso.");
         }
 
-        public static void LogarInformacao(string mensagem, params object[] args)
+        public static void LogarInformacao(string mensagem, params object?[] args)
         {
             Log.Information(mensagem, args);
         }
 
-        public static void LogarErro(string mensagem, params object[] args)
+        public static void LogarErro(string mensagem, params object?[] args)
         {
             Log.Error(mensagem, args);
         }
 
-        public static void LogarErroComException(Exception ex, string mensagem, params object[] args)
+        public static void LogarErroComException(Exception ex, string mensagem, params object?[] args)
         {
             Log.Error(ex, mensagem, args);
         }
 
-        public static void LogarAlerta(string mensagem, params object[] args)
+        public static void LogarAlerta(string mensagem, params object?[] args)
         {
             Log.Warning(mensagem, args);
         }
 
-        public static void LogarDebug(string mensagem, params object[] args)
+        public static void LogarDebug(string mensagem, params object?[] args)
         {
             Log.Debug(mensagem, args);
         }
 
-        public static void LogarTrace(string mensagem, params object[] args)
+        public static void LogarTrace(string mensagem, params object?[] args)
         {
             Log.Verbose(mensagem, args);
         }
 
-        public static void LogarCritico(string mensagem, params object[] args)
+        public static void LogarCritico(string mensagem, params object?[] args)
         {
             Log.Fatal(mensagem, args);
         }
@@ -62,4 +68,3 @@ namespace Telinha.Services
             Log.CloseAndFlush();
         }
     }
-}
