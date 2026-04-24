7d5a29323dacfe81b18d2571c133f8edb92829cd
Dreamless2 <tiagonet50@gmail.com>
2026-04-24 17:19:02 -0300

    Container.cs

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,35 @@
-﻿using System;
+﻿using Autofac;
+using Microsoft.Extensions.Caching.Memory;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Telinha.Factory;
+using Telinha.Services;
 
 namespace Telinha.Container
 {
-    internal class Container
+    private static IContainer BuildContainer()
     {
+        var builder = new ContainerBuilder();
+
+        // MemoryCache
+        builder.Register(c => new MemoryCache(new MemoryCacheOptions()))
+          .As<IMemoryCache>()
+          .SingleInstance();
+
+        // Cache híbrido
+        builder.RegisterType<FileCacheService>()
+          .AsSelf()
+          .SingleInstance();
+
+        // APIs
+        builder.RegisterType<ApiClientFactory>().AsSelf().SingleInstance();
+        builder.RegisterType<TMDBServices>().AsSelf();
+        builder.RegisterType<MidiaServices>().AsSelf();
+
+        // Forms
+        builder.RegisterType<Home>().AsSelf();
+
+        return builder.Build();
     }
 }
