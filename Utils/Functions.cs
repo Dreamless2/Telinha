847b63dad430c09e8ada00110a35caad58f9b7b8
Dreamless2 <tiagonet50@gmail.com>
2026-04-26 10:36:20 -0300

    EnumExtensions.cs

--- Utils/Functions.cs
+++ Utils/Functions.cs
@@ -5,9 +5,7 @@
         public static void OnlyNumbers(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
-            {
                 e.Handled = true;
-            }
         }
     }
 }
