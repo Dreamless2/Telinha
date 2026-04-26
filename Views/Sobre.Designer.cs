980860514d4dbc5c182e37516af64b4cd6873f01
Dreamless2 <tiagonet50@gmail.com>
2026-04-26 14:38:25 -0300

    Sobre.resx

--- Views/Sobre.Designer.cs
+++ Views/Sobre.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Sobre";
+            SuspendLayout();
+            // 
+            // Sobre
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(966, 577);
+            Name = "Sobre";
+            Text = "Sobre";
+            ResumeLayout(false);
         }
 
         #endregion
