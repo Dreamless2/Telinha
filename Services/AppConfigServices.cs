1864bec47706fef32cf424abc71d9bc1aaa01d07
Dreamless2 <tiagonet50@gmail.com>
2026-04-26 13:01:00 -0300

    AppConfigServices.cs

--- Services/AppConfigServices.cs
+++ Services/AppConfigServices.cs
@@ -22,7 +22,7 @@ namespace Telinha.Services
             public string? DEEPL { get; set; }
         }
 
-        public void Save(AppConfig config)
+        public static void Save(AppConfig config)
         {
             Directory.CreateDirectory(Path.GetDirectoryName(FilePath)!);
 
