9fb1072f599cf717e07fc6ec88b9cf391cda4dc8
Dreamless2 <tiagonet50@gmail.com>
2026-04-26 13:24:34 -0300

    AppConfigServices.cs

--- Cache/TranslationCache.cs
+++ Cache/TranslationCache.cs
@@ -3,7 +3,6 @@
     public static class TranslationCache
     {
         private static readonly Dictionary<string, string> Cache = new(StringComparer.OrdinalIgnoreCase);
-
         public static bool TryGet(string key, out string value)
             => Cache.TryGetValue(key, out value!);
 
