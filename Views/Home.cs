f339fd672d4e847137bd51f58e15d130ada73f33
Dreamless2 <tiagonet50@gmail.com>
2026-04-25 22:51:14 -0300

    Home.cs

--- Views/Home.cs
+++ Views/Home.cs
@@ -561,8 +561,8 @@ namespace Telinha
 
                 currentId = item.Id;
                 PreencherCampos(item);
-                _ = Enum.TryParse<MidiaTipo>(item.Tipo, out var tipoEnum);
-                AtualizarUI(tipoEnum, item);
+                if (Enum.TryParse<MidiaTipo>(item.Tipo, out var tipoEnum))
+                    AtualizarUI(tipoEnum, item);
                 _bs.Position = _bs.IndexOf(item);
                 await AtualizarBotoesNavegacao();
             }
