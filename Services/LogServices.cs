cd39c356ae2c923f6f6708b75ec7dabcc825ba86
Dreamless2 <tiagonet50@gmail.com>
2026-04-26 10:54:36 -0300

    MidiaServices.cs

--- Services/LogServices.cs
+++ Services/LogServices.cs
@@ -4,65 +4,29 @@ namespace Telinha.Services
 {
     public static class LogServices
     {
-        private static bool _configurado = false;
+        private static readonly Lazy<Logger> _lazyLogger = new(InitLogger);
 
-        public static void ConfigurarLog()
+        public static Logger Logger => _lazyLogger.Value;
+
+        private static Logger InitLogger()
         {
-            if (_configurado) return;
+            var logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "log-.txt");
 
-            string logPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs", "log-.txt");
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.File(logPath,
+            var logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .Enrich.FromLogContext() // permite ForContext
+               .WriteTo.File(logPath,
                     rollingInterval: RollingInterval.Day,
                     retainedFileCountLimit: 7,
                     fileSizeLimitBytes: 10_000_000,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
-                .CreateLogger();
+                    rollOnFileSizeLimit: true,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
+               .CreateLogger();
 
-            _configurado = true;
-            Log.Information("Sistema de logs inicializado com sucesso.");
+            logger.Information("Sistema de logs inicializado com sucesso.");
+            return logger;
         }
 
-        public static void LogarInformacao(string mensagem, params object?[] args)
-        {
-            Log.Information(mensagem, args);
-        }
-
-        public static void LogarErro(string mensagem, params object?[] args)
-        {
-            Log.Error(mensagem, args);
-        }
-
-        public static void LogarErroComException(Exception ex, string mensagem, params object?[] args)
-        {
-            Log.Error(ex, mensagem, args);
-        }
-
-        public static void LogarAlerta(string mensagem, params object?[] args)
-        {
-            Log.Warning(mensagem, args);
-        }
-
-        public static void LogarDebug(string mensagem, params object?[] args)
-        {
-            Log.Debug(mensagem, args);
-        }
-
-        public static void LogarTrace(string mensagem, params object?[] args)
-        {
-            Log.Verbose(mensagem, args);
-        }
-
-        public static void LogarCritico(string mensagem, params object?[] args)
-        {
-            Log.Fatal(mensagem, args);
-        }
-
-        public static void EncerrarLog()
-        {
-            Log.CloseAndFlush();
-        }
+        public static void EncerrarLog() => Log.CloseAndFlush();
     }
 }
