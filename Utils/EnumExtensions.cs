07af3adedbe8457290195c800be3e2a57f1c63e2
Dreamless2 <tiagonet50@gmail.com>
2026-04-25 23:00:53 -0300

    EnumExtensions.cs

--- Utils/EnumExtensions.cs
+++ Utils/EnumExtensions.cs
@@ -7,4 +7,23 @@ namespace Telinha.Utils
 {
     public static class EnumExtensions
     {
-        
+        public static T GetValueFromDescription<T>(string description) where T : Enum
+        {
+            foreach (var field in typeof(T).GetFields())
+            {
+                if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attr)
+                {
+                    if (attr.Description == description)
+                    {
+                        if (field.GetValue(null) != null)
+                            return (T)field.GetValue(null);
+                    }
+                }
+                if (field.Name == description)
+                {
+                    if (field.GetValue(null) != null)
+                        return (T)field.GetValue(null);
+                }
+            }
+            return default;
+        }
