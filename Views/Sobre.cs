68f65c6f1b3c861e6c0c6cd585ec2d854e0708de
Dreamless2 <tiagonet50@gmail.com>
2026-04-27 16:26:16 -0300

    Sobre.Designer.cs

--- Views/Sobre.cs
+++ Views/Sobre.cs
@@ -14,20 +14,6 @@ namespace Telinha.Views
         public Sobre()
         {
             InitializeComponent();
-            Load += Sobre_Load;
-        }
-
-        private void Sobre_Load(object sender, EventArgs e)
-        {
-            var restSharp = Assembly.Load("RestSharp");
-            var freeSQL = Assembly.Load("FreeSql");
-            var mysqlConnector = Assembly.Load("FreeSql.Provider.MySqlConnector");
-            var deeplNet = Assembly.Load("DeepL.Net");
-
-            RestSharpLabel.Text = $"RestSharp: {restSharp.GetName().Version}";
-            FreeSqlLabel.Text = $"FreeSQL: {freeSQL.GetName().Version}";
-            MySQLConnectorLabel.Text = $"MySqlConnector: {mysqlConnector.GetName().Version}";
-            DeeplNetLabel.Text = $"DeepLNet: {deeplNet.GetName().Version}";
-        }
+        }       
     }
 }
