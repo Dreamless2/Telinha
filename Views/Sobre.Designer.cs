9457abb495126d4492b2145c1f1ecbd6a8d86f78
Dreamless2 <tiagonet50@gmail.com>
2026-04-26 22:15:48 -0300

    Sobre.Designer.cs

--- Views/Sobre.Designer.cs
+++ Views/Sobre.Designer.cs
@@ -34,6 +34,9 @@
             label1 = new Label();
             panel1 = new Panel();
             label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
             PanelTopTitle.SuspendLayout();
             panel1.SuspendLayout();
             SuspendLayout();
@@ -79,6 +82,9 @@
             // 
             // panel1
             // 
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
             panel1.Controls.Add(label2);
             panel1.Dock = DockStyle.Fill;
             panel1.Location = new Point(0, 87);
@@ -89,11 +95,38 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(103, 68);
+            label2.Location = new Point(12, 43);
             label2.Name = "label2";
             label2.Size = new Size(38, 15);
             label2.TabIndex = 0;
             label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 1;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 109);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 2;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 142);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 3;
+            label5.Text = "label5";
             // 
             // Sobre
             // 
@@ -123,5 +156,8 @@
         private Label label1;
         private Panel panel1;
         private Label label2;
+        private Label label5;
+        private Label label4;
+        private Label label3;
     }
 }
