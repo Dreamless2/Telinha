14e188909470a7117d1cae283810d25f5373e72c
Dreamless2 <tiagonet50@gmail.com>
2026-04-26 23:01:29 -0300

    Sobre.Designer.cs

--- Views/Sobre.cs
+++ Views/Sobre.cs
@@ -13,6 +13,30 @@ namespace Telinha.Views
         public Sobre()
         {
             InitializeComponent();
+            Load += Sobre_Load;
+        }
+
+        private void Sobre_Load(object sender, EventArgs e)
+        {
+            // Configurações para o formulário "Sobre"
+            this.Text = "Sobre o Aplicativo";
+            this.Size = new Size(400, 300);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            // Adiciona um rótulo com informações sobre o aplicativo
+            Label lblInfo = new Label();
+            lblInfo.Text = "Este é um aplicativo de exemplo para demonstrar a tela 'Sobre'.\n" +
+                           "Desenvolvido por: Seu Nome\n" +
+                           "Versão: 1.0.0\n" +
+                           "Data de lançamento: 2024-06-01";
+            lblInfo.AutoSize = true;
+            lblInfo.Location = new Point(20, 20);
+            this.Controls.Add(lblInfo);
+            // Adiciona um botão para fechar a janela
+            Button btnFechar = new Button();
+            btnFechar.Text = "Fechar";
+            btnFechar.Location = new Point(150, 200);
+            btnFechar.Click += (s, args) => this.Close();
+            this.Controls.Add(btnFechar);
         }
     }
 }
