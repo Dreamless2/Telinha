392c4c118c237c8e5ea079e77348265fcc1949de
Tiago Pereira <tiagonet50@gmail.com>
2026-06-29 00:14:04 -0300

    Updates Link Label

--- Telinha.Forms/Views/Sobre.cs
+++ Telinha.Forms/Views/Sobre.cs
@@ -18,7 +18,20 @@ namespace Telinha.Views
 
         private void LinkTMDB_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-
+            try
+            {
+                this.LinkTMDB.LinkVisited = true;
+                string url = "https://www.themoviedb.org/";
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possível abrir o link: " + ex.Message);
+            }
         }
     }
 }
