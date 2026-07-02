62c1496c88d0544a2372bbcf5d7abdec74fcf4e3
Tiago Pereira <tiagonet50@gmail.com>
2026-07-02 18:27:29 -0300

    MidiaModel.cs

--- Telinha.Core/Models/MidiaModel.cs
+++ Telinha.Core/Models/MidiaModel.cs
@@ -9,11 +9,7 @@ namespace Telinha.Core.Models
     public class MidiaModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string? name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+        
         protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? name = null)
         {
             if (EqualityComparer<T>.Default.Equals(field, value))
