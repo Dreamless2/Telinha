758f1eb4789333a577f6f8efcb2c50fbfbf21db5
Tiago Pereira <tiagonet50@gmail.com>
2026-07-21 22:05:38 -0300

    MidiaModel.cs

--- Telinha.Core/Models/MidiaModel.cs
+++ Telinha.Core/Models/MidiaModel.cs
@@ -204,17 +204,6 @@ namespace Telinha.Core.Models
         [Column(IsIgnore = true)]
         public List<string>? ProdutorasLista { get; set; }
 
-        [Column(IsIgnore = true)]
-        public int Episodios { get; set; }
-
-        [Column(IsIgnore = true)]
-        public int DuracaoMedia { get; set; }
-
-        [Column(IsIgnore = true)]
-        public double Popularidade { get; set; }
-
-        [Column(IsIgnore = true)]
-        public int Votos { get; set; }
 
         [Column(IsIgnore = true)]
         public bool EhAnimacao =>
