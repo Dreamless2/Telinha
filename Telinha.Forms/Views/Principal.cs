ff4fe3d9c2e117a70dbd1d933d953a149798e0d9
Tiago Pereira <tiagonet50@gmail.com>
2026-07-22 17:52:14 -0300

    Home.cs

---
+++ Telinha.Forms/Views/Principal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Telinha.Forms.Views
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
