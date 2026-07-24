4445612762dd5aafca7485ae536fe8bc3baf0f50
Tiago Pereira <tiagonet50@gmail.com>
2026-07-24 14:37:36 -0300

    Sobre.cs

--- Telinha.Forms/Views/Sobre.cs
+++ Telinha.Forms/Views/Sobre.cs
@@ -4,34 +4,10 @@ namespace Telinha.Views
 {
     public partial class Sobre : Form
     {
-
         public Sobre()
         {
             InitializeComponent();
-
-        }
-
-        private void LinkTMDB_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            try
-            {
-                LinkTMDB.LinkVisited = true;
-                string url = "https://www.themoviedb.org/";
-                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
-                {
-                    FileName = url,
-                    UseShellExecute = true
-                });
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Não foi possível abrir o link: " + ex.Message);
-            }
-        }
-
-        private void SobreButton_Click(object sender, EventArgs e)
-        {
-            Hide();
-        }
+        }     
+        
     }
 }
