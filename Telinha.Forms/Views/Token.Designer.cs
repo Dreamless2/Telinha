f3ddf0449ae8e26e444d09305a74b32a626433cb
Dreamless2 <tiagonet50@gmail.com>
2026-07-28 20:49:35 -0300

    Token.Designer.cs

--- Telinha.Forms/Views/Token.Designer.cs
+++ Telinha.Forms/Views/Token.Designer.cs
@@ -81,7 +81,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(10, 68);
+            label2.Location = new Point(10, 66);
             label2.Name = "label2";
             label2.Size = new Size(61, 15);
             label2.TabIndex = 3;
