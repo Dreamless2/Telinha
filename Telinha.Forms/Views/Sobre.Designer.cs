ff76662734c9fe3a7d0dd09bf543ce81b425a30a
Dreamless2 <tiagonet50@gmail.com>
2026-07-28 19:57:56 -0300

    Sobre.Designer.cs

--- Telinha.Forms/Views/Sobre.Designer.cs
+++ Telinha.Forms/Views/Sobre.Designer.cs
@@ -89,7 +89,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(320, 479);
+            label2.Location = new Point(296, 502);
             label2.Name = "label2";
             label2.Size = new Size(630, 25);
             label2.TabIndex = 12;
