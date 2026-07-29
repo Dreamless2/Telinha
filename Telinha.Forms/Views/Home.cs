8cce90531c65d680b70918921db70972c0e8887b
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 16:21:57 -0300

    Home.cs

--- Telinha.Forms/Views/Home.cs
+++ Telinha.Forms/Views/Home.cs
@@ -316,9 +316,7 @@ namespace Telinha
             }
         }
         private Task AnteriorButton_ClickAsync() => NavegarAsync(id => MidiaController.GetPrevious<MidiaModel>(id), "Você chegou ao primeiro registro.", AnteriorButton);
-
-        private Task ProximoButton_ClickAsync() =>
-            NavegarAsync(id => MidiaController.GetNext<MidiaModel>(id), "Você chegou ao último registro.", ProximoButton);
+        private Task ProximoButton_ClickAsync() => NavegarAsync(id => MidiaController.GetNext<MidiaModel>(id), "Você chegou ao último registro.", ProximoButton);
 
         private async void BuscarMidia(object sender, KeyEventArgs e)
         {
