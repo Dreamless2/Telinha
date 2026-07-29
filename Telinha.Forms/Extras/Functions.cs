331f6e25e96e48441da36616d4ba0911ee2e9393
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 12:15:46 -0300

    Functions.cs

--- Telinha.Forms/Extras/Functions.cs
+++ Telinha.Forms/Extras/Functions.cs
@@ -2,7 +2,6 @@
 {
     public class Functions
     {
-
         public static void OnlyNumbers(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true;
