dd72658e0934ee22ea934748ec0578cd5d8e2e0b
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 16:22:45 -0300

    Home.cs

--- Telinha.Forms/Views/Home.cs
+++ Telinha.Forms/Views/Home.cs
@@ -156,7 +156,6 @@ namespace Telinha
 
             string audioValue = string.IsNullOrWhiteSpace(midia.Audio) ? "Dublado" : midia.Audio;
             if (!AudioBox.Items.Contains(audioValue)) AudioBox.Items.Add(audioValue);
-
             AudioBox.SelectedItem = audioValue;
         }
         private void PreencherMascara(MidiaTipo midiaTipo)
