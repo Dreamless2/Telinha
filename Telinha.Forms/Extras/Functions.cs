f31f1bf3e3c83e3ad6394ccce17719201de02318
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 12:15:34 -0300

    Functions.cs

--- Telinha.Forms/Extras/Functions.cs
+++ Telinha.Forms/Extras/Functions.cs
@@ -1,10 +1,10 @@
 ﻿namespace Telinha.Forms.Extras
 {
     public class Functions
-
+    {
 
         public static void OnlyNumbers(object sender, KeyPressEventArgs e)
-
+        { }
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) e.Handled = true;
 
 
