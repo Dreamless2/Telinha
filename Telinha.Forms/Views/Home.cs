f3669e9a9ba9eb21afa9f46419a2423d288c9752
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 19:15:35 -0300

    Home.cs

--- Telinha.Forms/Views/Home.cs
+++ Telinha.Forms/Views/Home.cs
@@ -93,7 +93,7 @@ namespace Telinha
             RadioAnimes.CheckedChanged += TypeRadio_CheckedChanged!;
         }
 
-        private void LimparApenasTextBoxesVazios(Control container)
+        private static void LimparApenasTextBoxesVazios(Control container)
         {
             foreach (Control controle in container.Controls)
             {
