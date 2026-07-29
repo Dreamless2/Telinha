796330e4cc5875c844d6411a3ceee986f390530f
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 12:16:02 -0300

    ContainerConfig.cs

--- Telinha.Forms/Container/ContainerConfig.cs
+++ Telinha.Forms/Container/ContainerConfig.cs
@@ -14,22 +14,17 @@ namespace Telinha.Forms.Container
             builder.Register(c => new MemoryCache(new MemoryCacheOptions()))
               .As<IMemoryCache>().SingleInstance();
             builder.RegisterType<AppConfigServices>().AsSelf().SingleInstance();
-
             builder.RegisterType<ApiClientFactory>().AsSelf().SingleInstance();
-
             builder.Register(c => c.Resolve<ApiClientFactory>().GetTMDB())
               .AsSelf()
               .SingleInstance();
-
             builder.Register(c => c.Resolve<ApiClientFactory>().GetDeepL())
               .AsSelf()
               .InstancePerDependency();
-
             builder.RegisterType<MidiaServices>().AsSelf();
             builder.RegisterType<Token>().AsSelf();
             builder.RegisterType<Home>().AsSelf();
             builder.RegisterType<Principal>().AsSelf();
-
             return builder.Build();
         }
     }
