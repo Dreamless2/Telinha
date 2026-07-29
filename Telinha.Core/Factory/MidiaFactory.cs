2ab3fc6542a1dd8ce304b07e699dc9820fdea262
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 12:23:58 -0300

    MidiaFactory.cs

--- Telinha.Core/Factory/MidiaFactory.cs
+++ Telinha.Core/Factory/MidiaFactory.cs
@@ -106,11 +106,8 @@ namespace Telinha.Core.Factory
             var languageIso = json["spoken_languages"]?.FirstOrDefault()?["iso_639_1"]?.ToString();
             var taskPais = !string.IsNullOrWhiteSpace(countryRaw) ? deepl.Translate(countryRaw) : Task.FromResult<string?>("--");
             var taskIdioma = LanguageHelper.ResolveAsync(languageEnglish, languageIso, deepl.Translate);
-
             await Task.WhenAll(taskPais!, taskIdioma);
-
             item.Local = TagEngine.FormatarTitulo(taskPais.Result ?? "--");
-
             item.Idioma = TagEngine
                 .FormatarTitulo(taskIdioma.Result ?? "--")
                 .ToLower();
