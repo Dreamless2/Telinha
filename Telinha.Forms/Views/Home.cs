013531e293dcb1b729715555ceda6c2ee087bccc
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 19:20:31 -0300

    Home.cs

--- Telinha.Forms/Views/Home.cs
+++ Telinha.Forms/Views/Home.cs
@@ -116,6 +116,7 @@ namespace Telinha
         {
             if (sender is RadioButton rb && !rb.Checked)
             {
+                if (!TemAlgumTextBoxPreenchido(this)))
                 ConfigurarTodosOsTextBoxes(this);
                 return;
             }
