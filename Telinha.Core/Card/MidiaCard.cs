e7ab4a61919a8acf1f8426e741d59871bf2262fd
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 12:20:01 -0300

    MidiaCard.cs

--- Telinha.Core/Card/MidiaCard.cs
+++ Telinha.Core/Card/MidiaCard.cs
@@ -55,9 +55,7 @@ namespace Telinha.Core.Card
             sb.AppendLine("**HD** - __720p__");
             sb.AppendLine("**SD** - __480p__");
             sb.AppendLine("(__Os vídeos estão em ordem decrescente, ou seja, de cima para baixo, tal como na descrição das resoluções.__)" + Environment.NewLine);
-
             sb.AppendLine($"**Sinopse:** __{_sinopse}__" + Environment.NewLine);
-
             sb.AppendLine($"**Nome Original:** __{_original}__");
             sb.AppendLine($"**Nome alternativo:** __{_alternativo}__");
             sb.AppendLine($"**Data de Estreia:** __{_lancamento}__");
