98a57b94014cf07bc8853992d32dfd3701114cb2
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 16:22:28 -0300

    Home.cs

--- Telinha.Forms/Views/Home.cs
+++ Telinha.Forms/Views/Home.cs
@@ -404,7 +404,6 @@ namespace Telinha
 
                 var (inserted, updated) = await MidiaController.SaveAsync(_current);
                 MessageBox.Show(inserted ? $"{_current.Nome} inserido com sucesso!" : $"{_current.Nome} atualizado com sucesso!");
-
                 CarregarNaTela(_current);
                 await AtualizarBotoesNavegacao();
             }
