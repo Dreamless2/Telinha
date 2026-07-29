20fb0e3a3ad3ab7aeade83f2981b2445e46df2a6
Dreamless2 <tiagonet50@gmail.com>
2026-07-29 12:01:45 -0300

    MidiaModel.cs

--- Telinha.Core/Models/MidiaModel.cs
+++ Telinha.Core/Models/MidiaModel.cs
@@ -10,6 +10,7 @@ namespace Telinha.Core.Models
     {
         public event PropertyChangedEventHandler? PropertyChanged;
         protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? name = null)
+
         {
             if (EqualityComparer<T>.Default.Equals(field, value))
                 return false;
