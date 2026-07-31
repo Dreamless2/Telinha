a4420dfa24ee5a65fa06c2996d4add9191ef60fe
Dreamless2 <tiagonet50@gmail.com>
2026-07-31 09:20:55 -0300

    Database.cs

--- Telinha.Core/Data/Database.cs
+++ Telinha.Core/Data/Database.cs
@@ -32,12 +32,7 @@ namespace Telinha.Core.Data
             var config = _configService.Load() ?? throw new InvalidOperationException("Configuração não encontrada.");
             if (string.IsNullOrWhiteSpace(config.Host) || string.IsNullOrWhiteSpace(config.Porta) || string.IsNullOrWhiteSpace(config.Usuario) || string.IsNullOrWhiteSpace(config.Senha)) throw new InvalidOperationException("Configuração inválida.");
 
-            _connStr =
-               $"Data Source={config.Host};" +
-                $"Port={config.Porta};" +
-                $"User ID={config.Usuario};" +
-                $"Password={config.Senha};" +
-                $"Initial Catalog=telinha;";
+            _connStr = $"Data Source={config.Host};" + $"Port={config.Porta};" + $"User ID={config.Usuario};" + $"Password={config.Senha};" + $"Initial Catalog=telinha;";
 
             return _connStr;
         }
