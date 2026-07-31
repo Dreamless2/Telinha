a5e7c06f8e46f83ed3f6f4099049f4fe325936e1
Dreamless2 <tiagonet50@gmail.com>
2026-07-31 11:37:05 -0300

    MidiaFactory.cs

--- Telinha.Forms/Views/Home.cs
+++ Telinha.Forms/Views/Home.cs
@@ -186,7 +186,10 @@ namespace Telinha
                     txt.Leave += TextBox_Leave!;
                     if (string.IsNullOrWhiteSpace(txt.Text)) txt.Text = "--";
                 }
-                else if (c.HasChildren) ConfigurarTodosOsTextBoxes(c);
+                else if (c.HasChildren)
+                {
+                    ConfigurarTodosOsTextBoxes(c);
+                }
             }
         }
         private void ConectarEventos()
