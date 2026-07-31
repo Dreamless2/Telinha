506445e44faa9d334bd5117f30358ad87cf46ecb
Dreamless2 <tiagonet50@gmail.com>
2026-07-31 14:46:20 -0300

    Home.cs

--- Telinha.Forms/Views/Home.cs
+++ Telinha.Forms/Views/Home.cs
@@ -84,6 +84,9 @@ namespace Telinha
             RadioSeries.CheckedChanged += TypeRadio_CheckedChanged!;
             RadioAnimes.CheckedChanged += TypeRadio_CheckedChanged!;
         }
+
+        private void Limpar
+
         private void TypeRadio_CheckedChanged(object sender, EventArgs e)
         {
             if (sender is RadioButton rb && !rb.Checked)
