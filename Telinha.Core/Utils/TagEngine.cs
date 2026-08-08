c6ee75b691e5cfa04a22f08c39a8aac07928d4b5
Dreamless2 <tiagonet50@gmail.com>
2026-08-07 22:56:35 -0300

    TagEngine.cs

--- Telinha.Core/Utils/TagEngine.cs
+++ Telinha.Core/Utils/TagEngine.cs
@@ -68,7 +68,8 @@ namespace Telinha.Core.Utils
                     var chaveNormalizada = chave.ToLowerInvariant();
                     var semAcento = RemoverAcentos(chave);
                     hashTags.Add($"#{semAcento}");
-                    if (!semAcento.Equals(chave, StringComparison.OrdinalIgnoreCase)) hashTags.Add($"#{chave}");
+                    if (!semAcento.Equals(chave, StringComparison.OrdinalIgnoreCase))
+                        hashTags.Add($"#{chave}");
                 }
             }
 
